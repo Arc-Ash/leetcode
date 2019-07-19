32d1d2aac754e32f698637648c5b8e48be3f4c95
Arc-Ash <53069928+Arc-Ash@users.noreply.github.com>
2019-07-19 17:06:08 +0800

    Update 1_TwoSum.py

--- python3/1_TwoSum.py
+++ python3/1_TwoSum.py
@@ -5,3 +5,23 @@ class solution(object):
                 if nums[i] + nums[j] == target:
                     return [i,j]
 print(solution.twosum([3,2,4],6))
+#速度最快：
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) <= 1:
+            return False
+        keys = {}
+        for i, v in enumerate(nums):
+            if target-v in keys:
+                return [keys[target-v],i]
+            else:
+                keys[v] = i
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.twoSum([2,7,11,15],9))
