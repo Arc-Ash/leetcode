38748cadfc0bd2eafd7bf7245caeb6db1da8648a
Arc-Ash <53069928+Arc-Ash@users.noreply.github.com>
2019-07-29 20:19:33 +0800

    Create 75_sortColors.py

---
+++ python3/75_sortColors.py
@@ -0,0 +1,16 @@
+75_sortColors.py
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i,j,k = 0,len(nums)-1,0
+        while k<len(nums):
+            if nums[k] ==0 and k>i:
+                nums[k],nums[i]=nums[i],nums[k]
+                i += 1
+            elif nums[k] == 2 and k<j:
+                nums[k],nums[j]=nums[j],nums[k]
+                j -= 1
+            else:
+                k += 1
